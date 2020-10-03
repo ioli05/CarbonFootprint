af1ae2c177a5c75a8651da042e5456f14b3e3c40
Tudor Paraschivescu <tudorp97@gmail.com>
2020-10-04 02:06:12 +0300

    Sort feed in descending order

--- app/src/main/java/com/example/carbonfootprint/helpers/NewsfeedModelComparator.java
+++ app/src/main/java/com/example/carbonfootprint/helpers/NewsfeedModelComparator.java
@@ -7,13 +7,6 @@ import java.util.Comparator;
 public class NewsfeedModelComparator implements Comparator<NewsfeedModel> {
     @Override
     public int compare(NewsfeedModel nf1, NewsfeedModel nf2) {
-        long t1 = nf1.getTimestamp().getTime();
-        long t2 = nf2.getTimestamp().getTime();
-        if(t2 < t1)
-            return 1;
-        else if(t1 < t2)
-            return -1;
-        else
-            return 0;
+        return (int) (nf2.getTimestamp().getTime() - nf1.getTimestamp().getTime());
     }
 }
