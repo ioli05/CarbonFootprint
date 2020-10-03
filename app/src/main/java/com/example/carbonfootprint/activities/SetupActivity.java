52dfe9b1adb55600f3d8b60f7335cf591d3c387b
Tudor Paraschivescu <tudorp97@gmail.com>
2020-10-03 16:25:22 +0300

    Enhance Setup screen

--- app/src/main/java/com/example/carbonfootprint/activities/SetupActivity.java
+++ app/src/main/java/com/example/carbonfootprint/activities/SetupActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.carbonfootprint.R;
 import com.example.carbonfootprint.helpers.Auth;
+import com.example.carbonfootprint.tabset.MainActivity;
 
 public class SetupActivity extends AppCompatActivity {
 
+    private RadioGroup fuelTypeRg;
+    private RadioGroup carSizeRg;
+    private EditText electricityEt;
     private Button nextBtn;
 
     @Override
@@ -19,12 +28,52 @@ public class SetupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setup);
         findActivityViews();
 
-        Intent intent = new Intent(this, ProfileActivity.class);
-        nextBtn.setOnClickListener(view -> startActivity(intent));
+        nextBtn.setOnClickListener(view -> {
+            String fuelType = getCheckedOptionFuelType(fuelTypeRg.getCheckedRadioButtonId());
+            String carSize = getCheckedOptionCarSize(carSizeRg.getCheckedRadioButtonId());
+            int electricity = Integer.parseInt(electricityEt.getText().toString());
+            Toast.makeText(this, "TODO: Use values\n" + fuelType + " ; " + carSize + " ; " + electricity, Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 
     private void findActivityViews() {
         nextBtn = findViewById(R.id.next_btn);
+        fuelTypeRg = findViewById(R.id.fuel_type_rg);
+        carSizeRg = findViewById(R.id.car_size_rg);
+        electricityEt = findViewById(R.id.electricity_et);
+    }
+
+    private String getCheckedOptionFuelType(int checkedId) {
+        switch (checkedId) {
+            case R.id.fuel_type_rg_1:
+                return "Petrol";
+            case R.id.fuel_type_rg_2:
+                return "Diesel";
+            case R.id.fuel_type_rg_3:
+                return "Hybrid";
+            case R.id.fuel_type_rg_4:
+                return "Electric";
+            default:
+                return "";
+        }
+    }
+
+    private String getCheckedOptionCarSize(int checkedId) {
+        switch (checkedId) {
+            case R.id.car_size__rg_1:
+                return "Compact";
+            case R.id.car_size__rg_2:
+                return "Medium";
+            case R.id.car_size__rg_3:
+                return "Minivan";
+            case R.id.car_size__rg_4:
+                return "SUV";
+            default:
+                return "";
+        }
     }
 
 }
