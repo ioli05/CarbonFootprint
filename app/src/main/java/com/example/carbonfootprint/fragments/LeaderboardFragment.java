9d8ca9216027d688ddc6e7f31b315cda0b653fa2
ioli05 <ionela.nicuta05@gmail.com>
2020-10-03 15:45:41 +0300

    Added navigation bottom bar

---
+++ app/src/main/java/com/example/carbonfootprint/fragments/LeaderboardFragment.java
@@ -0,0 +1,26 @@
+package com.example.carbonfootprint.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.carbonfootprint.R;
+
+public class LeaderboardFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
